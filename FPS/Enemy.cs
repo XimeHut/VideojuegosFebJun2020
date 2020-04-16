679a2b7780643e59533fb8ddf7d3b9f5e6964d1d
XimeHut <54177898+XimeHut@users.noreply.github.com>
2020-04-16 18:09:52 -0500

    Add files via upload

---
+++ FPS/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject enemy; //El objeto que persigue.
+    public GameObject track; //El que va ser perseguido. (En este caso es el FPS)
+    //Estas variables son publicas para que se modifiquen dependiendo de las caractersisticas que quieran que tenga su enemigo
+    //Recomendamos : MaxDist: 20
+                   //MinDist:  5
+                   //velocity : 0.05
+    public float MaxDist = 20f; //Es la distancia en la que te empieza a seguir * te sigue en un radio de 20 a 5 *
+    public float MinDist = 5f; //Es la distancia hasta la que se te pega.
+    public float velocity = 0.05f;//Es la velocidad a la que te sigue 
+
+    private Vector3 EnemyPos;
+    private Vector3 TrackPos;
+    private float Distancia;
+    
+
+    public void EnemyMovement()
+    {
+        EnemyPos = enemy.transform.position;// Es la posicion del enemigo
+        TrackPos = track.transform.position; //Es la posicion de a quien se le va seguir (En este caso es el FPS)
+
+        Distancia = Vector3.Distance(EnemyPos, TrackPos); //Distancia entre ellos 
+
+        Debug.Log(Distancia);
+
+        if (Distancia >= MinDist && Distancia <= MaxDist)
+        {
+            enemy.transform.position = Vector3.MoveTowards(EnemyPos, TrackPos, velocity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();
+    }
+}
---
+++ FPS/FPS.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+    //GameObject del FPS
+    public GameObject fps;
+
+    //Variables para el movimiento de la vista 
+    public float mouseSensitive = 10f;
+    private float mouseX;
+    private float mouseY;
+    private float rotationX;
+
+    //Variables para el movimiento
+
+    float speed;
+    public float walk = 10f; //la velocidad con la que camina normal el fps
+    public float run = 30f; //la velocidad con la que corre el fps
+
+    public float jumpHeight = 2f;
+    
+    private float moves2s;
+    private float movefb;
+    private float moveud;
+    private float gravity = -9.81f;
+    private bool isGrounded;
+
+    private Vector3 movimiento;
+    private CharacterController controller;
+  
+
+    //Variables para un ground más estable
+
+    public GameObject magicSphere;
+    public LayerMask groundMask;
+    private float radious = 0.2f;
+    
+
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = fps.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        FPSlook();
+        FPSMove();
+    }
+
+    public void FPSMove()
+    {
+        speed = 10f;
+
+
+        isGrounded = Physics.CheckSphere(magicSphere.transform.position, radious, groundMask);
+        
+        
+        if (isGrounded)
+            moveud = 0;
+        else
+            moveud += gravity * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            moveud = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        if (Input.GetButton("Run") && Input.GetAxis("Vertical") != 0) //En este caso se aprieta la letra de "R" para modificar la velocidad.
+                                                                      //Esta se configura en Edit --> Project Settings -->  Input Manager --> 
+                                                                      //Agregas una con el nombre "Run" y eliges la letra o boton con el que quieres activar esta funcion . (En este caso es "r")
+        {
+            speed = run;
+        }
+        else
+        {
+            speed = walk;
+        }
+
+
+        movefb = Input.GetAxis("Vertical");
+        moves2s = Input.GetAxis("Horizontal");
+
+        movimiento = ((fps.transform.right * moves2s) +
+                     (fps.transform.up * moveud) +
+                     (fps.transform.forward * movefb));
+        movimiento = movimiento * speed * Time.deltaTime;
+        controller.Move(movimiento);
+
+        
+
+
+
+
+    }
+
+    public void FPSlook()
+    {
+        mouseX = Input.GetAxis("Mouse X")*mouseSensitive*Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y")*mouseSensitive*Time.deltaTime;
+        
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        fps.transform.Rotate(Vector3.up, mouseX);
+    }
+
+
+}
